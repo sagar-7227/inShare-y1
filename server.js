cd745f360c888245c9054fbc10c1cb4717757c9e
sagar <sagarvashnav72427@gmail.com>
2021-09-11 19:25:20 +0530

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express =require('express');
 const app =express();
 const path =require('path');
+const cors =require('cors');
+
 
 const PORT =process.env.PORT ||3000;
 app.use(express.static('public'));
@@ -10,6 +12,14 @@ app.use(express.json());
 
 const connectDB =require('./config/db');
 connectDB();
+
+// cors
+const corsOptions ={
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    
+}
+
+app.use(cors(corsOptions));
 
 // template engine
 app.set('views',path.join(__dirname,'/views'));
